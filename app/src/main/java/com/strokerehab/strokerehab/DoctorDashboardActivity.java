02d6a975bf8164e84d9d1631329dad32202c4779
joyendra <joyendraroybiswas@gmail.com>
2019-03-31 01:16:56 +0530

    Activites UI improved

---
+++ app/src/main/java/com/strokerehab/strokerehab/DoctorDashboardActivity.java
@@ -0,0 +1,49 @@
+package com.strokerehab.strokerehab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.AppCompatTextView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toolbar;
+
+import java.util.ArrayList;
+
+public class DoctorDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_dashboard);
+
+        centerTitle();
+
+    }
+
+    private void centerTitle() {
+        ArrayList<View> textViews = new ArrayList<>();
+
+        getWindow().getDecorView().findViewsWithText(textViews, getTitle(), View.FIND_VIEWS_WITH_TEXT);
+
+        if (textViews.size() > 0) {
+            AppCompatTextView appCompatTextView = null;
+            if (textViews.size() == 1) {
+                appCompatTextView = (AppCompatTextView) textViews.get(0);
+            } else {
+                for (View v : textViews) {
+                    if (v.getParent() instanceof Toolbar) {
+                        appCompatTextView = (AppCompatTextView) v;
+                        break;
+                    }
+                }
+            }
+
+            if (appCompatTextView != null) {
+                ViewGroup.LayoutParams params = appCompatTextView.getLayoutParams();
+                params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+                appCompatTextView.setLayoutParams(params);
+                appCompatTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            }
+        }
+    }
+}
--- app/src/main/java/com/strokerehab/strokerehab/LoginActivity.java
+++ app/src/main/java/com/strokerehab/strokerehab/LoginActivity.java
@@ -11,40 +11,43 @@ import android.widget.Toast;
 
 public class LoginActivity extends Activity {
 
-    EditText muserName;
+    EditText mUserName;
     EditText mPassWord;
     Button loginButton, signUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_login );
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
-        muserName = findViewById( R.id.mobile_field );
-        mPassWord = findViewById( R.id.password_field );
+        mUserName = findViewById(R.id.mobile_field);
+        mPassWord = findViewById(R.id.password_field);
 
-        loginButton = findViewById( R.id.login_button );
-        signUpButton = findViewById( R.id.signin_button );
+        loginButton = findViewById(R.id.login_button);
+        signUpButton = findViewById(R.id.signin_button);
 
-        loginButton.setOnClickListener( new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (muserName.getText().toString().equals( "pat" ) && mPassWord.getText().toString().equals( "123" )) {
-                    startActivity( new Intent( LoginActivity.this, MainActivity.class ) );
+                if (mUserName.getText().toString().equals("pat") && mPassWord.getText().toString().equals("123")) {
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    finishAffinity();
+                } else if (mUserName.getText().toString().equals("doc") && mPassWord.getText().toString().equals("123")) {
+                    startActivity(new Intent(LoginActivity.this, DoctorDashboardActivity.class));
                     finishAffinity();
                 } else {
-                    Toast.makeText( LoginActivity.this, "Unable to Login", Toast.LENGTH_SHORT ).show();
+                    Toast.makeText(LoginActivity.this, "Unable to Login", Toast.LENGTH_SHORT).show();
                 }
             }
-        } );
+        });
 
-        signUpButton.setOnClickListener( new View.OnClickListener() {
+        signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity( new Intent( LoginActivity.this, SignActivity.class ) );
+                startActivity(new Intent(LoginActivity.this, SignActivity.class));
 
             }
-        } );
+        });
 
 
     }
