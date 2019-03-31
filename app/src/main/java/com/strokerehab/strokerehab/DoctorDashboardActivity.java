b532b52cd9e4e1be663c3c05275691a44530f808
joyendra <joyendraroybiswas@gmail.com>
2019-03-31 10:29:38 +0530

    Effort slider and minor changes

--- app/src/main/java/com/strokerehab/strokerehab/DoctorDashboardActivity.java
+++ app/src/main/java/com/strokerehab/strokerehab/DoctorDashboardActivity.java
@@ -1,55 +1,94 @@
 package com.strokerehab.strokerehab;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatTextView;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.Toast;
 import android.widget.Toolbar;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class DoctorDashboardActivity extends AppCompatActivity {
     LinearLayout graphLL, chatLL, patientsLL;
-    // 95403
-    //  25118
+    SeekBar effortSeekBar;
+    Integer currentValue;
+    FirebaseDatabase database;
+    DatabaseReference mDatabaseRef;
+    ImageView logoutImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_dashboard);
 
-        centerTitle();
-
         graphLL = findViewById(R.id.ll_real_time_graph);
         chatLL = findViewById(R.id.ll_chat);
         patientsLL = findViewById(R.id.ll_guide);
-    }
 
-    private void centerTitle() {
-        ArrayList<View> textViews = new ArrayList<>();
+        database = FirebaseDatabase.getInstance();
+        mDatabaseRef = database.getReference();
 
-        getWindow().getDecorView().findViewsWithText(textViews, getTitle(), View.FIND_VIEWS_WITH_TEXT);
+        effortSeekBar = findViewById(R.id.sb_effort);
+        currentValue = effortSeekBar.getProgress();
 
-        if (textViews.size() > 0) {
-            AppCompatTextView appCompatTextView = null;
-            if (textViews.size() == 1) {
-                appCompatTextView = (AppCompatTextView) textViews.get(0);
-            } else {
-                for (View v : textViews) {
-                    if (v.getParent() instanceof Toolbar) {
-                        appCompatTextView = (AppCompatTextView) v;
-                        break;
-                    }
+        logoutImageView = findViewById(R.id.iv_logout);
+
+        effortSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onProgressChanged(final SeekBar seekBar, int progress, boolean fromUser) {
+                currentValue = progress;
+                Toast.makeText(DoctorDashboardActivity.this, "" + currentValue, Toast.LENGTH_SHORT).show();
+
+                try {
+                    mDatabaseRef.child("devices").child("effort").setValue(currentValue);
+                } catch (Exception e) {
+                    Log.e("TAG", e.getMessage());
                 }
+
             }
 
-            if (appCompatTextView != null) {
-                ViewGroup.LayoutParams params = appCompatTextView.getLayoutParams();
-                params.width = ViewGroup.LayoutParams.MATCH_PARENT;
-                appCompatTextView.setLayoutParams(params);
-                appCompatTextView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
             }
-        }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        graphLL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DoctorDashboardActivity.this, GraphActivity.class));
+            }
+        });
+
+        chatLL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DoctorDashboardActivity.this, ChatActivity.class));
+            }
+        });
+
+        logoutImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DoctorDashboardActivity.this, LoginActivity.class));
+                finishAffinity();
+            }
+        });
     }
+
 }
